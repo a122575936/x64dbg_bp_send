b53f76b622198e681da76dc9368487d24a402057
tinyli <lww202@126.com>
2023-05-24 14:15:54 +0800

    Update x64dbg.cmake

--- cmake/x64dbg.cmake
+++ cmake/x64dbg.cmake
@@ -1,7 +1,7 @@
 CPMAddPackage(
     NAME x64dbg
     URL https://downloads.sourceforge.net/project/x64dbg/snapshots/snapshot_2023-05-12_12-32.zip
-    URL_HASH SHA1=C75AC6BA7E7E5AB60632EED5648254E4BEF11465
+    URL_HASH SHA1=8ba3d8961767774c934a710ea799cb89b8e2e488
     DOWNLOAD_ONLY ON
 )
 
